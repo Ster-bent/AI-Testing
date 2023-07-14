ceb64d9fa86e3932c29e84af0803dbbfd956d9f6
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2023-07-14 06:54:21 +0000

    algorith classification problems

--- ai_learningAlgorithm/ai_Learning_algorithm.py
+++ ai_learningAlgorithm/ai_Learning_algorithm.py
@@ -1,9 +1,12 @@
 #ai learning algorithm
 #will add new command/keyword
 #need to create a method that can determine if a string is equal to a certain phrase like hi=greeting
+danger_class = ["help", "me", "9", "oh no","1"]
+cmd_class = ["/", "&", "!"]
+greet_class = ["h","H","i","I","e","l","o","y"]
+big_list = [greet_class, cmd_class, danger_class]
 
-greet_class = ["h","H","i","I","e","l","o"]
-
+#not for list testing
 greetings = []
 #can be used
 def create_list(listtype, directory):
@@ -12,6 +15,11 @@ def create_list(listtype, directory):
     for i in range(len(contents)):
         listtype.append(contents[i])
     f.close()
+
+
+
+
+
 #beta of classifcation program
 def classify(z):
     listz = []
@@ -19,12 +27,21 @@ def classify(z):
     classification = any(item in greet_class for item in listz)
     if classification is True:
         z = "greeting"
-    return z
-
+#NEW classify program beta0.1
+def classify2(uinput):
+    uinput_list = []
+    uinput_list[:0] = uinput
+    for i in range(len(big_list)):
+        simalarity = len(set(uinput_list) and set(big_list[i])) / float(len(set(uinput) or set(big_list[i]))) *10
+        if simalarity >= 15:
+            print(f"Type: {big_list[i]}")
+            print(f"Simalarity: {simalarity}")
+        else:
+            pass
 #ready for use
 def learn(lesson, type):
     if type == "greeting":
         memory = open("greetings.txt", "a")
         memory.write(lesson)
 
-print(classify("sam"))
+classify2("911")

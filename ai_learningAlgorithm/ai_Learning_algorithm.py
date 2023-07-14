06ec17524ff2039d848361e64d8ba2a6ad79cb7d
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2023-07-14 06:07:31 +0000

    algorithm beta made

--- ai_learningAlgorithm/ai_Learning_algorithm.py
+++ ai_learningAlgorithm/ai_Learning_algorithm.py
@@ -1,24 +1,30 @@
 #ai learning algorithm
 #will add new command/keyword
 #need to create a method that can determine if a string is equal to a certain phrase like hi=greeting
+
+greet_class = ["h","H","i","I","e","l","o"]
+
 greetings = []
+#can be used
 def create_list(listtype, directory):
     f = open(directory, "r")
-    contents = f.readlines
-    count = 0
-    while count >= contents:
-        listtype.append(contents[count])
-        count += 1
+    contents = f.readlines()
+    for i in range(len(contents)):
+        listtype.append(contents[i])
     f.close()
+#beta of classifcation program
+def classify(z):
+    listz = []
+    listz[:0] = z
+    classification = any(item in greet_class for item in listz)
+    if classification is True:
+        z = "greeting"
+    return z
 
-def classify(Uinput):
-    if Uinput in greetings :
-        Uinput = "greeting"
-
+#ready for use
 def learn(lesson, type):
     if type == "greeting":
         memory = open("greetings.txt", "a")
         memory.write(lesson)
 
-create_list(greetings, "greetings.txt")
-print(greetings)
+print(classify("sam"))
